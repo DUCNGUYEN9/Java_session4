768d9c6c707c9972fe58f605f81c95f801789c3b
ducnguyenngoc <106985979+DUCNGUYEN9@users.noreply.github.com>
2023-07-24 21:32:02 +0900

    Bai9.java

--- src/Bai9.java
+++ src/Bai9.java
@@ -5,7 +5,7 @@ public class Bai9 {
         //Tìm phần tử lớn thứ 2 trong Mảng
         int[] arrInt = {1, 6, 2, -8, 17, 3};
         int max = arrInt[0];
-        int max2 = 0;
+        int max2 = arrInt[0];
         for (int i = 0; i < arrInt.length; i++) {
             if (max < arrInt[i]) {
                 max2 = max;
